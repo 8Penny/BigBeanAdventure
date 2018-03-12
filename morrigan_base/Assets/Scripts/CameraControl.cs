5fbec6400c1d94391fb90201aca055ea4edb90a1
8penny <iampolya@ya.ru>
2018-03-12 23:57:38 +0300

    Fight + New Collider

--- morrigan_base/Assets/Scripts/CameraControl.cs
+++ morrigan_base/Assets/Scripts/CameraControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 
     private GameObject player;
-    private Vector3 offset = new Vector3(0, 0, -10);
+    private Vector3 offset = new Vector3(0, 2, -10);
 
     private void Start()
     {
--- morrigan_base/Assets/Scripts/PlayerMovement.cs
+++ morrigan_base/Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,15 @@ public class PlayerMovement : MonoBehaviour {
     Animator animator;
 
     private bool isGrounded;
+
     private bool JumpRequest;
+    
     public float horSpeed = 0.1f;
     float speedX;
     public float verImp = 6f;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+    private bool attack;
 
     void Start()
     {
@@ -27,15 +30,17 @@ public class PlayerMovement : MonoBehaviour {
     {
         if (Input.GetButtonDown("Jump") && isGrounded) // вкл прыжок
             { JumpRequest = true; }
+        if (Input.GetButtonDown("Fire1"))
+            { attack = true; Fight(); }
     }
 
     void FixedUpdate() {
-        if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") < 0)
+        if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") < 0 )
         { speedX = -horSpeed; playerSP.flipX = true; animator.SetBool("Move", true); } // задаём направление движения и
-        else if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") > 0)
+        else if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") > 0 )
         { speedX = horSpeed; playerSP.flipX = false; animator.SetBool("Move", true); } // направление взгляда и вкл анимацию бега
         else { animator.SetBool("Move", false); }
-        transform.Translate(speedX, 0, 0); // движение
+        player.transform.Translate(speedX, 0, 0); // движение
         speedX = 0; // обнуляем скорость движения, когда его не должно быть
 
         if (JumpRequest) // выполнение прыжка
@@ -51,12 +56,32 @@ public class PlayerMovement : MonoBehaviour {
             animator.SetBool("Fall", true); // при падении вкл анимацию падения
         }
         else if (player.velocity.y >= 0) { animator.SetBool("Fall", false); }
+
+        attack = false;
     }
 
     void OnCollisionEnter2D(Collision2D col) // проверка прикосновения с землёй, и если земля, то сбрасываем анимцию прыжка
-    { if (col.gameObject.tag == "Ground") { isGrounded = true; animator.ResetTrigger("Jump"); } }
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true; animator.ResetTrigger("Jump");
+        }
+
+    }
 
     void OnCollisionExit2D(Collision2D colex) // проверка "покидания" земли
-    { if (colex.gameObject.tag == "Ground") { isGrounded = false; } }
+    {
+        if (colex.gameObject.tag == "Ground" )
+        { isGrounded = false;  }
 
+    }
+
+
+
+    void Fight()
+    {
+        if (attack)
+        { animator.SetTrigger("Fight"); }
+
+    }
 }
