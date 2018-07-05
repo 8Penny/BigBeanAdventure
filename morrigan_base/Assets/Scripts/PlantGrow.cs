4c36133597da0f845a647adc602968806d539149
8penny <iampolya@ya.ru>
2018-07-05 23:56:37 +0500

    grow plant added

---
+++ morrigan_base/Assets/Scripts/PlantGrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGrow : MonoBehaviour
+{
+
+    Animator animator;
+    public bool isPlantGrown = false;
+    public bool isPlayer = false;
+    public Collider2D player;
+    public Collider2D plant;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        plant = GetComponent<Collider2D>();
+        player = GameObject.FindWithTag("Player").GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        CheckPlayer();
+        if (!isPlantGrown && isPlayer && Input.GetKeyDown(KeyCode.E)) { animator.SetBool("plant", true); isPlantGrown = true; print("You've got GOLD!"); }
+    }
+
+    void CheckPlayer()
+    { isPlayer = Physics2D.IsTouching(plant, player); }
+
+}
